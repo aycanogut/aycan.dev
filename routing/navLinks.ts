2f4ad229861f786e471a7601d04b7f338860f84f
Aycan Ogut <aycanonfire@gmail.com>
2022-05-30 16:24:35 +0300

    feat: :sparkles: add routing and navigation links

---
+++ routing/navLinks.ts
@@ -0,0 +1,20 @@
+import { urls } from './urls'
+
+export const navLinks = [
+    {
+        link: urls.Home,
+        label: 'Home',
+    },
+    {
+        link: urls.Projects,
+        label: 'Projects',
+    },
+    {
+        link: urls.Blog,
+        label: 'Blog',
+    },
+    {
+        link: urls.About,
+        label: 'About',
+    },
+]
