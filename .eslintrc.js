b4f3f6ebf03a2a851a981f1cdd004ce96bfc9794
Aycan Ogut <aycanonfire@gmail.com>
2022-05-30 18:18:47 +0300

    chore: :wrench: add configs

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    extends: [
+        'mantine',
+        'plugin:@next/next/recommended',
+        'plugin:jest/recommended',
+        'plugin:storybook/recommended',
+    ],
+    plugins: ['testing-library', 'jest'],
+    overrides: [
+        {
+            files: ['**/?(*.)+(spec|test).[jt]s?(x)'],
+            extends: ['plugin:testing-library/react'],
+        },
+    ],
+    parserOptions: {
+        project: './tsconfig.json',
+    },
+    rules: {
+        'react/react-in-jsx-scope': 'off',
+        semi: 'off',
+        '@typescript-eslint/semi': 'off',
+    },
+}
