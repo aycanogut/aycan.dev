66214e7b43f92da4120900234baae2f3d17b8aa9
Aycan Ogut <aycanonfire@gmail.com>
2022-06-02 23:07:23 +0300

    chore: :wrench: add no-unused-vars rule to iff

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,25 @@
 module.exports = {
-    extends: [
-        'mantine',
-        'plugin:@next/next/recommended',
-        'plugin:jest/recommended',
-        'plugin:storybook/recommended',
-    ],
-    plugins: ['testing-library', 'jest'],
-    overrides: [
-        {
-            files: ['**/?(*.)+(spec|test).[jt]s?(x)'],
-            extends: ['plugin:testing-library/react'],
-        },
-    ],
-    parserOptions: {
-        project: './tsconfig.json',
+  extends: [
+    'mantine',
+    'plugin:@next/next/recommended',
+    'plugin:jest/recommended',
+    'plugin:storybook/recommended',
+  ],
+  plugins: ['testing-library', 'jest'],
+  overrides: [
+    {
+      files: ['**/?(*.)+(spec|test).[jt]s?(x)'],
+      extends: ['plugin:testing-library/react'],
     },
-    rules: {
-        'react/react-in-jsx-scope': 'off',
-        semi: 'off',
-        '@typescript-eslint/semi': 'off',
-    },
+  ],
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  rules: {
+    semi: 'off',
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/semi': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'no-unused-vars': 'off',
+  },
 }
