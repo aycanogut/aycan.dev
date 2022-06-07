6a4933828239d590714135182bb714e680ebe7b2
Aycan Ogut <aycanonfire@gmail.com>
2022-06-07 16:51:58 +0300

    refactor: :coffin: remove fragment

--- pages/about.tsx
+++ pages/about.tsx
@@ -2,11 +2,9 @@ import { Container } from '@mantine/core'
 import Layout from '../components/Layout/Layout'
 
 const About = () => (
-  <>
-    <Layout>
-      <Container size="md">About</Container>
-    </Layout>
-  </>
+  <Layout>
+    <Container size="md">About</Container>
+  </Layout>
 )
 
 export default About
--- pages/blog.tsx
+++ pages/blog.tsx
@@ -2,11 +2,9 @@ import { Container } from '@mantine/core'
 import Layout from '../components/Layout/Layout'
 
 const Blog = () => (
-  <>
-    <Layout>
-      <Container size="md">Blog</Container>
-    </Layout>
-  </>
+  <Layout>
+    <Container size="md">Blog</Container>
+  </Layout>
 )
 
 export default Blog
