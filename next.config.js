3bbbbeca8212de3e1517313b586ad5ca89d17a67
Aycan Ogut <aycanonfire@gmail.com>
2022-06-08 17:49:25 +0300

    chore: :wrench: add image domains

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
-});
+})
 
 module.exports = withBundleAnalyzer({
   reactStrictMode: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
-});
+  images: {
+    domains: ['www.simplilearn.com', 'cdn-images-1.medium.com'],
+  },
+})
