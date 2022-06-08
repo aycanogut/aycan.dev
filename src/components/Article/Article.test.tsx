6f7790442e498cf5079dafb74917c215787bfe03
Aycan Ogut <aycanonfire@gmail.com>
2022-06-08 12:28:15 +0300

    refactor: :truck: update Component name && add empty props

--- src/components/Article/Article.test.tsx
+++ src/components/Article/Article.test.tsx
@@ -1,9 +1,9 @@
 import { render, screen } from '@testing-library/react'
-import Card from './Article'
+import Article from './Article'
 
 describe('Welcome component', () => {
   it('has correct Next.js theming section link', () => {
-    render(<Card thumbnail={''} title={''} />)
+    render(<Article thumbnail={''} title={''} link={''} categories={[]} />)
     expect(screen.getByText('this guide')).toHaveAttribute(
       'href',
       'https://mantine.dev/theming/next/'
