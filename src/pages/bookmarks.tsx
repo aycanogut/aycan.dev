d2b61a18da84525d9d40f0a8aca30952d2bebb1f
Aycan Ogut <aycanonfire@gmail.com>
2022-06-10 11:55:32 +0300

    feat: :sparkles: add bookmarks page

--- src/pages/bookmarks.tsx
+++ src/pages/bookmarks.tsx
@@ -1,10 +1,47 @@
+import { FC } from 'react'
 import { Title } from '@mantine/core'
 import Layout from '../components/Layout/Layout'
+import Bookmark, { IBookmarkProps } from '../components/Bookmark/Bookmark'
 
-const Blog = () => (
+interface IBookmarksProps {
+  bookmarks: {
+    items: []
+  }
+}
+
+const Bookmarks: FC<IBookmarksProps> = ({ bookmarks }) => (
   <Layout>
-    <Title order={1}>Bookmarks that I liked all around the web</Title>
+    <Title order={1} mb={30}>
+      Bookmarks that I liked all around the web
+    </Title>
+    {bookmarks.items.map((bookmark: IBookmarkProps) => (
+      <Bookmark
+        key={bookmark._id}
+        title={bookmark.title}
+        link={bookmark.link}
+      />
+    ))}
   </Layout>
 )
 
-export default Blog
+export async function getServerSideProps() {
+  const res = await fetch(
+    'https://api.raindrop.io/rest/v1/raindrops/23246795',
+    {
+      method: 'get',
+      headers: {
+        Authorization: `Bearer ${process.env.RAINDROP_TOKEN}`,
+      },
+    }
+  )
+
+  const bookmarks = await res.json()
+
+  return {
+    props: {
+      bookmarks,
+    },
+  }
+}
+
+export default Bookmarks
