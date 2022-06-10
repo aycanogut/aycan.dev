3cdaf764ced7484ef9e11c44e677da00d82d6978
Aycan Ogut <aycanonfire@gmail.com>
2022-06-10 18:08:36 +0300

    feat: :poop: add bookmarks page && data

--- src/pages/bookmarks.tsx
+++ src/pages/bookmarks.tsx
@@ -1,7 +1,8 @@
-import { FC } from 'react'
-import { Title } from '@mantine/core'
+import { FC, useState } from 'react'
+import { SegmentedControl, Title } from '@mantine/core'
 import Layout from '../components/Layout/Layout'
 import Bookmark, { IBookmarkProps } from '../components/Bookmark/Bookmark'
+import { bookmarkIds } from '../components/Bookmark/bookmarkIds'
 
 interface IBookmarksProps {
   bookmarks: {
@@ -9,24 +10,44 @@ interface IBookmarksProps {
   }
 }
 
-const Bookmarks: FC<IBookmarksProps> = ({ bookmarks }) => (
-  <Layout>
-    <Title order={1} mb={30}>
-      Bookmarks that I liked all around the web
-    </Title>
-    {bookmarks.items.map((bookmark: IBookmarkProps) => (
-      <Bookmark
-        key={bookmark._id}
-        title={bookmark.title}
-        link={bookmark.link}
+const Bookmarks: FC<IBookmarksProps> = ({ bookmarks }) => {
+  const [value, setValue] = useState<string>('')
+
+  return (
+    <Layout>
+      <Title order={1} mb={30}>
+        Bookmarks that I liked all around the web
+      </Title>
+      <SegmentedControl
+        aria-label="Tabs"
+        value={value}
+        data={[
+          { value: 'blogs', label: 'Blogs' },
+          { value: 'js', label: 'JavaScript' },
+          { value: 'react', label: 'React' },
+          { value: 'webdev', label: 'Web Development' },
+          { value: 'design', label: 'Design' },
+          { value: 'recipes', label: 'Recipes' },
+        ]}
+        onChange={setValue}
+        mb={30}
+        sx={{ width: '100%' }}
       />
-    ))}
-  </Layout>
-)
+      {bookmarks.items.map((bookmark: IBookmarkProps) => (
+        <Bookmark
+          key={bookmark._id}
+          title={bookmark.title}
+          link={bookmark.link}
+        />
+      ))}
+      {console.log(bookmarks)}
+    </Layout>
+  )
+}
 
 export async function getServerSideProps() {
   const res = await fetch(
-    'https://api.raindrop.io/rest/v1/raindrops/23246795',
+    `https://api.raindrop.io/rest/v1/raindrops/${bookmarkIds.blogs}`,
     {
       method: 'get',
       headers: {
