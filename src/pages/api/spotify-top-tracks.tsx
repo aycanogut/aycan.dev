d164f81c12bcf0f362958fc84a185fa7f1980f13
Aycan Ogut <aycanonfire@gmail.com>
2022-06-11 16:19:07 +0300

    feat: :sparkles: add spotify api features

---
+++ src/pages/api/spotify-top-tracks.tsx
@@ -0,0 +1,25 @@
+// https://github.com/leerob/leerob.io/blob/main/pages/api/top-tracks.ts
+
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { getTopTracks } from '../../lib/spotify'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const response = await getTopTracks()
+  const { items } = await response.json()
+
+  const tracks = items.slice(0, 10).map((track) => ({
+    artist: track.artists.map((_artist) => _artist.name).join(', '),
+    songUrl: track.external_urls_spotify,
+    title: track.name,
+  }))
+
+  res.setHeader(
+    'Cache-Control',
+    'public, s-maxage-86400, stale-while-revalidate=43200'
+  )
+
+  return res.status(200).json({ tracks })
+}
