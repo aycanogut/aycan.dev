f757f48383a51f4f952f3f1527aa488ac1ef07d3
Aycan Ogut <aycanonfire@gmail.com>
2022-06-14 15:21:26 +0300

    feat: :sparkles: add filter feature through segmented control && update api url

--- src/pages/bookmarks.tsx
+++ src/pages/bookmarks.tsx
@@ -2,7 +2,6 @@ import { FC, useState } from 'react'
 import { SegmentedControl, Title } from '@mantine/core'
 import Layout from '../components/Layout/Layout'
 import Bookmark, { IBookmarkProps } from '../components/Bookmark/Bookmark'
-import { bookmarkIds } from '../components/Bookmark/bookmarkIds'
 
 interface IBookmarksProps {
   bookmarks: {
@@ -10,8 +9,16 @@ interface IBookmarksProps {
   }
 }
 
+interface IFilteredBookmarksProps {
+  tags: [string]
+}
+
 const Bookmarks: FC<IBookmarksProps> = ({ bookmarks }) => {
-  const [value, setValue] = useState<string>('')
+  const [value, setValue] = useState<string>('blog')
+  const filteredBookmarks = bookmarks.items.filter(
+    (filteredBookmark: IFilteredBookmarksProps) =>
+      filteredBookmark.tags[0] === value
+  )
 
   return (
     <Layout>
@@ -22,46 +29,47 @@ const Bookmarks: FC<IBookmarksProps> = ({ bookmarks }) => {
         aria-label="Tabs"
         value={value}
         data={[
-          { value: 'blogs', label: 'Blogs' },
-          { value: 'js', label: 'JavaScript' },
+          { value: 'blog', label: 'Blogs' },
+          { value: 'talk', label: 'Talks' },
+          { value: 'javascript', label: 'JavaScript' },
           { value: 'react', label: 'React' },
-          { value: 'webdev', label: 'Web Development' },
+          { value: 'webdev', label: 'Web Dev' },
           { value: 'design', label: 'Design' },
-          { value: 'recipes', label: 'Recipes' },
         ]}
         onChange={setValue}
         mb={30}
         sx={{ width: '100%' }}
+        transitionDuration={300}
       />
-      {/* {bookmarks.items.map((bookmark: IBookmarkProps) => (
+      {filteredBookmarks.map((bookmark: IBookmarkProps) => (
         <Bookmark
           key={bookmark._id}
           title={bookmark.title}
           link={bookmark.link}
         />
-      ))} */}
+      ))}
     </Layout>
   )
 }
 
-// export async function getServerSideProps() {
-//   const res = await fetch(
-//     'https://api.raindrop.io/rest/v1/raindrops/25244220',
-//     {
-//       method: 'get',
-//       headers: {
-//         Authorization: `Bearer ${process.env.RAINDROP_TOKEN}`,
-//       },
-//     }
-//   )
+export async function getServerSideProps() {
+  const res = await fetch(
+    'https://api.raindrop.io/rest/v1/raindrops/0?perpage=50&collectionId=25244220',
+    {
+      method: 'get',
+      headers: {
+        Authorization: `Bearer ${process.env.RAINDROP_TOKEN}`,
+      },
+    }
+  )
 
-//   const bookmarks = await res.json()
+  const bookmarks = await res.json()
 
-//   return {
-//     props: {
-//       bookmarks,
-//     },
-//   }
-// }
+  return {
+    props: {
+      bookmarks,
+    },
+  }
+}
 
 export default Bookmarks
