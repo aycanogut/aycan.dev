2be279efc5481ee0ae68621a9f3217cfba74e83c
Aycan Ogut <aycanonfire@gmail.com>
2022-06-20 21:12:52 +0300

    test: :test_tube: update function to fix production fetch bug

--- src/lib/raindrop.js
+++ src/lib/raindrop.js
@@ -3,10 +3,12 @@ const token_endpoint =
   'https://api.raindrop.io/rest/v1/raindrops/0?perpage=50&collectionId=25244220'
 
 export const getBookmarks = async () => {
-  return fetch(token_endpoint, {
+  const response = await fetch(token_endpoint, {
     method: 'GET',
     headers: {
       Authorization: `Bearer ${token}`,
     },
   })
+  const data = await response.json()
+  return data
 }
