5e2c82c524f349f0dfee18c0f29e2987ed82ee46
Aycan Ogut <aycanonfire@gmail.com>
2022-06-22 13:34:38 +0300

    refactor: :alien: update response name and return method

--- src/lib/raindrop.ts
+++ src/lib/raindrop.ts
@@ -3,14 +3,12 @@ const token_endpoint =
   'https://api.raindrop.io/rest/v1/raindrops/0?perpage=50&collectionId=25244220'
 
 export const getBookmarks = async () => {
-  const res = await fetch(token_endpoint, {
+  const response = await fetch(token_endpoint, {
     method: 'GET',
     headers: {
       Authorization: `Bearer ${token}`,
     },
   })
 
-  const data = await res.json()
-
-  return data.items
+  return response.json()
 }
