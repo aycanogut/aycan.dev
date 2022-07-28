e1b8be3c55d4aa29ed3425ba2880a22ad1052413
Aycan Ogut <aycanonfire@gmail.com>
2022-07-28 18:57:59 +0300

    feat: :lipstick: add backgroundColor and hover bg

--- src/components/Project/Project.styles.ts
+++ src/components/Project/Project.styles.ts
@@ -1,6 +1,22 @@
 import { createStyles } from '@mantine/core'
 
 export default createStyles((theme) => ({
+  card: {
+    ...theme.fn.focusStyles(),
+
+    backgroundColor:
+      theme.colorScheme === 'dark'
+        ? theme.colors.dark[6]
+        : theme.colors.gray[0],
+
+    '&:hover': {
+      backgroundColor:
+        theme.colorScheme === 'dark'
+          ? theme.colors.dark[7]
+          : theme.colors.gray[1],
+    },
+  },
+
   imageWrapper: {
     [theme.fn.smallerThan('xs')]: {
       textAlign: 'center',
