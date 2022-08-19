f64faae77a7d59842f5cc45e84ebf8be0a66705e
Aycan Ogut <aycanonfire@gmail.com>
2022-08-19 14:36:40 +0300

    fix: :lipstick: add hover transitions and update hover bg-colors

--- src/components/Bookmark/Bookmark.styles.ts
+++ src/components/Bookmark/Bookmark.styles.ts
@@ -8,17 +8,18 @@ export default createStyles((theme) => ({
       theme.colorScheme === 'dark'
         ? theme.colors.yellow[4]
         : theme.colors.violet[8],
-
     backgroundColor:
       theme.colorScheme === 'dark'
         ? theme.colors.dark[6]
         : theme.colors.gray[0],
+    transition: '.2s ease-in-out',
 
     '&:hover': {
       backgroundColor:
         theme.colorScheme === 'dark'
-          ? theme.colors.dark[7]
+          ? theme.colors.dark[5]
           : theme.colors.gray[1],
+      transition: '.2s ease-in-out',
     },
   },
 }))
--- src/components/Project/Project.styles.ts
+++ src/components/Project/Project.styles.ts
@@ -8,12 +8,14 @@ export default createStyles((theme) => ({
       theme.colorScheme === 'dark'
         ? theme.colors.dark[6]
         : theme.colors.gray[0],
+    transition: '.2s ease-in-out ',
 
     '&:hover': {
       backgroundColor:
         theme.colorScheme === 'dark'
-          ? theme.colors.dark[7]
+          ? theme.colors.dark[5]
           : theme.colors.gray[1],
+      transition: '.2s ease-in-out ',
     },
   },
 
