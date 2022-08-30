bdc6783470f8d6dbc210c72a04396ae9d2e6a02a
Aycan Ogut <aycanonfire@gmail.com>
2022-08-30 16:30:37 +0300

    feat: :sparkles: add error component

---
+++ src/components/Error/Error.test.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from '@testing-library/react'
+import Error from './Error'
+
+describe('Welcome component', () => {
+  it('has correct Next.js theming section link', () => {
+    render(<Error />)
+    expect(screen.getByText('this guide')).toHaveAttribute(
+      'href',
+      'https://mantine.dev/theming/next/'
+    )
+  })
+})
