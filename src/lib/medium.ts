996a2d129bc24ff7bf47a05a489ef5dd86bf86c9
Aycan Ogut <aycanonfire@gmail.com>
2022-09-08 13:59:40 +0300

    refactor: :recycle: update variable names

--- src/lib/medium.ts
+++ src/lib/medium.ts
@@ -1,10 +1,10 @@
+const token_endpoint =
+  'https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@aycanogut'
+
 export const getBlogPosts = async () => {
-  const response = await fetch(
-    'https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@aycanogut',
-    {
-      method: 'GET',
-    }
-  )
+  const response = await fetch(token_endpoint, {
+    method: 'GET',
+  })
 
   return response.json()
 }
