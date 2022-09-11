c4c565be132eb2db574cffa926eb86d66b9f02fc
Aycan Ogut <aycanonfire@gmail.com>
2022-09-11 23:17:08 +0300

    fix: :lipstick: remove transition

--- src/components/Bookmark/Bookmark.styles.ts
+++ src/components/Bookmark/Bookmark.styles.ts
@@ -6,18 +6,17 @@ export default createStyles((theme) => ({
       theme.colorScheme === 'dark'
         ? theme.colors.yellow[4]
         : theme.colors.violet[8],
+
     backgroundColor:
       theme.colorScheme === 'dark'
         ? theme.colors.dark[6]
         : theme.colors.gray[0],
-    transition: '.2s ease-in-out',
 
     '&:hover': {
       backgroundColor:
         theme.colorScheme === 'dark'
           ? theme.colors.dark[5]
           : theme.colors.gray[1],
-      transition: '.2s ease-in-out',
     },
 
     ...theme.fn.focusStyles(),
