89c361cd575dd89d1bfd343d56d8b78160ebd8e9
Aycan Ogut <aycanonfire@gmail.com>
2022-09-12 00:45:19 +0300

    feat: :sparkles: add motion animation

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,5 +1,6 @@
 import { FC } from 'react'
 import { Container } from '@mantine/core'
+import { motion } from 'framer-motion'
 import CustomHeader from '../CustomHeader/CustomHeader'
 import { ILayoutProps } from '../../interfaces/Layout.interface'
 import { navLinks } from '../CustomHeader/navLinks'
@@ -9,7 +10,14 @@ const Layout: FC<ILayoutProps> = ({ children }) => (
     <CustomHeader links={navLinks} />
     <main>
       <Container size="md" px="xl" mb={60}>
-        {children}
+        <motion.div
+          initial={{ y: 16 }}
+          animate={{ y: 0 }}
+          exit={{ y: 16 }}
+          transition={{ duration: 0.18 }}
+        >
+          {children}
+        </motion.div>
       </Container>
     </main>
   </>
