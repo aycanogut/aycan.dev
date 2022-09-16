95d588454901fdb5593b30acfc023408db8a2cd8
Aycan Ogut <aycanonfire@gmail.com>
2022-09-16 17:43:03 +0300

    feat: :lipstick: add hover animation effect with motion

--- src/components/Bookmark/Bookmark.tsx
+++ src/components/Bookmark/Bookmark.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from 'react'
 import { Card, Text } from '@mantine/core'
+import { motion } from 'framer-motion'
 import { IBookmarkProps } from '../../interfaces/Bookmark.interface'
 import useStyles from './Bookmark.styles'
 
@@ -7,19 +8,24 @@ const Bookmark: FC<IBookmarkProps> = ({ title, link }) => {
   const { classes } = useStyles()
 
   return (
-    <Card
-      className={classes.card}
-      component="a"
-      href={link}
-      target="_blank"
-      withBorder
-      radius="sm"
-      p="sm"
-      mb={14}
-      shadow="xs"
+    <motion.div
+      whileHover={{ scale: 1.01 }}
+      transition={{ type: 'spring', stiffness: 100, damping: 10 }}
     >
-      <Text weight={600}>{title}</Text>
-    </Card>
+      <Card
+        className={classes.card}
+        component="a"
+        href={link}
+        target="_blank"
+        withBorder
+        radius="sm"
+        p="sm"
+        mb={14}
+        shadow="xs"
+      >
+        <Text weight={600}>{title}</Text>
+      </Card>
+    </motion.div>
   )
 }
 
--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -9,6 +9,7 @@ import {
   Button,
   useMantineTheme,
 } from '@mantine/core'
+import { motion } from 'framer-motion'
 import NextImage from 'next/future/image'
 import { IProjectProps } from '../../interfaces/Project.interface'
 import useStyles from './Project.styles'
@@ -24,76 +25,81 @@ const Project: FC<IProjectProps> = ({
   const theme = useMantineTheme()
 
   return (
-    <Card
-      className={classes.card}
-      withBorder
-      radius="sm"
-      p="lg"
-      mb={40}
-      shadow="md"
+    <motion.div
+      whileHover={{ scale: 1.01 }}
+      transition={{ type: 'spring', stiffness: 100, damping: 10 }}
     >
-      <Grid>
-        <Grid.Col span={12} xs={4} className={classes.imageWrapper}>
-          <NextImage
-            src={image}
-            alt={`Image of ${title} project`}
-            width="160"
-            height="160"
-          />
-        </Grid.Col>
-        <Grid.Col span={12} xs={8}>
-          <Group>
-            <Title order={3}>{title}</Title>
-            <Text>{description}</Text>
-          </Group>
-          <Group mt={28}>
-            <Text weight={700} size="sm">
-              Tech Stack:
-            </Text>
-            {stack &&
-              stack.map((tech: string, i: number) => (
-                <Badge
-                  key={i}
-                  color={theme.colorScheme === 'dark' ? 'yellow' : 'grape'}
-                  size="xs"
-                  variant="outline"
-                >
-                  {tech}
-                </Badge>
-              ))}
-          </Group>
-          <Group mt={22} spacing="sm">
-            <Text weight={700} size="sm">
-              Links:
-            </Text>
-            {links &&
-              links.map(
-                (link: { name: string; link: string }, index: number) => (
-                  <Button
-                    key={index}
-                    variant="default"
-                    color="yellow"
+      <Card
+        className={classes.card}
+        withBorder
+        radius="sm"
+        p="lg"
+        mb={40}
+        shadow="md"
+      >
+        <Grid>
+          <Grid.Col span={12} xs={4} className={classes.imageWrapper}>
+            <NextImage
+              src={image}
+              alt={`Image of ${title} project`}
+              width="160"
+              height="160"
+            />
+          </Grid.Col>
+          <Grid.Col span={12} xs={8}>
+            <Group>
+              <Title order={3}>{title}</Title>
+              <Text>{description}</Text>
+            </Group>
+            <Group mt={28}>
+              <Text weight={700} size="sm">
+                Tech Stack:
+              </Text>
+              {stack &&
+                stack.map((tech: string, i: number) => (
+                  <Badge
+                    key={i}
+                    color={theme.colorScheme === 'dark' ? 'yellow' : 'grape'}
                     size="xs"
+                    variant="outline"
                   >
-                    <Text
-                      className={classes.button}
-                      component="a"
-                      variant="link"
-                      href={link.link}
-                      weight="700"
+                    {tech}
+                  </Badge>
+                ))}
+            </Group>
+            <Group mt={22} spacing="sm">
+              <Text weight={700} size="sm">
+                Links:
+              </Text>
+              {links &&
+                links.map(
+                  (link: { name: string; link: string }, index: number) => (
+                    <Button
+                      key={index}
+                      variant="default"
+                      color="yellow"
                       size="xs"
-                      target="_blank"
-                      transform="capitalize"
                     >
-                      {link.name}
-                    </Text>
-                  </Button>
-                )
-              )}
-          </Group>
-        </Grid.Col>
-      </Grid>
-    </Card>
+                      <Text
+                        className={classes.button}
+                        component="a"
+                        variant="link"
+                        href={link.link}
+                        weight="700"
+                        size="xs"
+                        target="_blank"
+                        transform="capitalize"
+                      >
+                        {link.name}
+                      </Text>
+                    </Button>
+                  )
+                )}
+            </Group>
+          </Grid.Col>
+        </Grid>
+      </Card>
+    </motion.div>
   )
 }
 
