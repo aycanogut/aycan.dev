bc30c2bd17b86babb46b4c314de63a84e5c6560d
Aycan Ogut <aycanonfire@gmail.com>
2022-09-17 22:35:12 +0300

    feat: :lipstick: add flexbox && minHeight

--- src/components/Project/Project.styles.ts
+++ src/components/Project/Project.styles.ts
@@ -2,10 +2,16 @@ import { createStyles } from '@mantine/core'
 
 export default createStyles((theme) => ({
   card: {
+    display: 'flex',
+    flexDirection: 'column',
     backgroundColor:
       theme.colorScheme === 'dark'
         ? theme.colors.dark[6]
         : theme.colors.gray[0],
+
+    [theme.fn.largerThan('sm')]: {
+      minHeight: 230,
+    },
 
     ...theme.fn.focusStyles(),
   },
