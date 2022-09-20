e92e77d792fa9d51cc2aaf7cf6457eb6d83e037f
Aycan Ogut <aycanonfire@gmail.com>
2022-09-20 13:23:41 +0300

    chore: :fire: remove strapi

--- backend/src/api/project/routes/project.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * project router.
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::project.project');
--- backend/src/api/project/services/project.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * project service.
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::project.project');
