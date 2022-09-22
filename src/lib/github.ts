435997b8bdcded8167a5f364aaf90ab3c60dc2e0
Aycan Ogut <aycanonfire@gmail.com>
2022-09-22 18:35:56 +0300

    refactor: :recycle: update fetch method with swr

--- src/lib/github.ts
+++
@@ -1,9 +0,0 @@
-const token_endpoint = process.env.GITHUB_URL
-
-export const getRepos = async () => {
-  const response = await fetch(token_endpoint, {
-    method: 'GET',
-  })
-
-  return response.json()
-}
