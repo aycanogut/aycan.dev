76e9d5842eaf648ad05f8f5e1914504fa6af03b6
Aycan Ogut <aycanonfire@gmail.com>
2022-10-12 20:47:24 +0300

    refactor: :sparkles: add icons && move spotlight content to own file

--- src/components/Spotlight/Spotlight.tsx
+++ src/components/Spotlight/Spotlight.tsx
@@ -2,23 +2,69 @@ import { FC } from 'react'
 import { useRouter } from 'next/router'
 import { useMantineColorScheme } from '@mantine/core'
 import { SpotlightAction, SpotlightProvider } from '@mantine/spotlight'
-import { SpotlightContent } from '../../helpers/spotlight'
+import {
+  Search,
+  Home2,
+  AppWindow,
+  Article,
+  Bookmarks,
+  Pacman,
+  Sun,
+  Moon,
+} from 'tabler-icons-react'
 import { ISpotlightProps } from '../../interfaces/Spotlight.interface'
 
 const Spotlight: FC<ISpotlightProps> = ({ children }) => {
   const router = useRouter()
-  const { toggleColorScheme } = useMantineColorScheme()
+  const { colorScheme, toggleColorScheme } = useMantineColorScheme()
+
+  const SpotlightContent = [
+    {
+      title: 'Home',
+      icon: <Home2 />,
+      url: '/',
+    },
+    {
+      title: 'Projects',
+      icon: <AppWindow />,
+      url: '/projects',
+    },
+    {
+      title: 'Articles',
+      icon: <Article />,
+      url: '/articles',
+    },
+    {
+      title: 'Bookmarks',
+      icon: <Bookmarks />,
+      url: '/bookmarks',
+    },
+    {
+      title: 'About',
+      icon: <Pacman />,
+      url: '/about',
+    },
+    {
+      title: 'Toggle Theme',
+      icon: colorScheme === 'dark' ? <Sun /> : <Moon />,
+    },
+  ]
 
   const actions: SpotlightAction[] = SpotlightContent?.map(
-    (item: { title: string; url: string }) => ({
+    (item: { title: string; icon: any; url: string }) => ({
       title: item.title,
+      icon: item.icon,
       onTrigger: item.url
-        ? () => router.push(`/${item.url}`)
+        ? () => router.push(`${item.url}`)
         : () => toggleColorScheme(),
     })
   )
 
-  return <SpotlightProvider actions={actions}>{children}</SpotlightProvider>
+  return (
+    <SpotlightProvider searchIcon={<Search />} actions={actions}>
+      {children}
+    </SpotlightProvider>
+  )
 }
 
 export default Spotlight
