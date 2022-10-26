83aa44e2c69af22284a7035d54b9ee25dfde0664
Aycan Ogut <aycanonfire@gmail.com>
2022-10-26 18:32:07 +0300

    refactor: :egg: add collections helper object

---
+++ src/helpers/collections.ts
@@ -0,0 +1,10 @@
+export const collections = {
+  books: '28080348',
+  podcasts: '25589596',
+  talks: '25244242',
+  blogs: '25244220',
+  design: '25244195',
+  webdev: '25244192',
+  react: '25244191',
+  js: '25244190',
+}
