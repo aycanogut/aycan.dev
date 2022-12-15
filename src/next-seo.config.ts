2d314bd3b65075b82aff2cdd06fbea4b7e78ff57
Aycan Ogut <aycanonfire@gmail.com>
2022-12-15 17:13:06 +0300

    refactor: :coffin: remove default canonical URL

--- src/next-seo.config.ts
+++ src/next-seo.config.ts
@@ -3,7 +3,6 @@ import { DefaultSeoProps } from 'next-seo'
 const SEO: DefaultSeoProps = {
   title: 'aycan.dev',
   description: 'The portfolio website of Aycan Öğüt, a software developer.',
-  canonical: 'https://www.aycan.dev',
   openGraph: {
     url: 'https://www.aycan.dev',
     title: 'aycan.dev',
