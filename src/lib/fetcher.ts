f754ac4f539c8e1218cdbf9e2ae13474191dbbc4
Aycan Ogut <aycanonfire@gmail.com>
2022-12-24 16:38:24 +0300

    fix: :bug: update fetcher method from SSR to CSR

--- src/lib/fetcher.ts
+++ src/lib/fetcher.ts
@@ -1,7 +1,3 @@
-export default async function fetcher<JSON = any>(
-  input: RequestInfo,
-  init?: RequestInit
-): Promise<JSON> {
-  const res = await fetch(input, init)
-  return res.json()
-}
+const fetcher = (url: string) => fetch(url).then((res) => res.json())
+
+export default fetcher
